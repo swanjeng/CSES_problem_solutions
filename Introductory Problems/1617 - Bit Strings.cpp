1c1d07d9b5a64e8e6245f263a58200782b4e334b
swanjeng <swanjeng1205@gmail.com>
2026-07-13 15:51:42 +0800

    Fix function name error

--- Introductory Problems/1617 - Bit Strings.cpp
+++ Introductory Problems/1617 - Bit Strings.cpp
@@ -5,9 +5,9 @@ using namespace std;
 LL fast_pow_mod(LL a, LL b) {
     if (b == 0) return 1;
     if (b == 1) return a % M;
-    if (b % 2 == 1) return (fp(a, b-1) % M * (a % M)) % M;
+    if (b % 2 == 1) return (fast_pow_mod(a, b-1) % M * (a % M)) % M;
     else {
-        LL t = fp(a, b/2)%M;
+        LL t = fast_pow_mod(a, b / 2) % M;
         return t*t%M;
     }
 }
